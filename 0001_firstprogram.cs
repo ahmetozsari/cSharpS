827f629d6543fadf2c86deba0760554a94b7239f
ahmetozsari <ahmetozsarieng@gmail.com>
2021-01-22 22:30:34 +0300

    Create 0001_firstprogram.cs
    first_cs_program

---
+++ 0001_firstprogram.cs
@@ -0,0 +1,21 @@
+// 00001 C# Program
+
+class Program
+{
+ static void Main(string[] args)
+ {
+ int i = 0;
+ int j = 2;
+ 
+ System.Console.WriteLine("i=" + i);
+ System.Console.WriteLine("j=" + j);
+ }
+}
+
+
+// The output:
+
+
+// i=0
+// j=2
+// The code above defines two variables and output their values.
